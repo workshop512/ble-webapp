522b28e6d5461c1eed10f3cf66de6da91c67d871
workshop512 <wrkshp512@gmail.com>
2021-03-21 19:36:45 -0500

    Update ble-test.js

--- ble-test.js
+++ ble-test.js
@@ -1,7 +1,50 @@
+function onButtonClick() {
+  let filters = [];
+
+  let filterService = document.querySelector('#service').value;
+  if (filterService.startsWith('0x')) {
+    filterService = parseInt(filterService);
+  }
+  if (filterService) {
+    filters.push({services: [filterService]});
+  }
+
+  let filterName = document.querySelector('#name').value;
+  if (filterName) {
+    filters.push({name: filterName});
+  }
+
+  let filterNamePrefix = document.querySelector('#namePrefix').value;
+  if (filterNamePrefix) {
+    filters.push({namePrefix: filterNamePrefix});
+  }
+
+  let options = {};
+  if (document.querySelector('#allDevices').checked) {
+    options.acceptAllDevices = true;
+  } else {
+    options.filters = filters;
+  }
+
+  log('Requesting Bluetooth Device...');
+  log('with ' + JSON.stringify(options));
+  navigator.bluetooth.requestDevice(options)
+  .then(device => {
+    log('> Name:             ' + device.name);
+    log('> Id:               ' + device.id);
+    log('> Connected:        ' + device.gatt.connected);
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+
+
 //get a reference to the element
 var scanBtn = document.getElementById('scanButton');
 
 //add event listener
 scanBtn.addEventListener('click', function(event) {
-  window.location.href='Students.html';
+  onButtonClick();
 });
