8a2e91530fd6e8fb7b45a3d285f057661ce10fbd
workshop512 <wrkshp512@gmail.com>
2021-03-21 19:28:26 -0500

    Create ble-test.js

---
+++ ble-test.js
@@ -0,0 +1,7 @@
+//get a reference to the element
+var scanBtn = document.getElementById('scanButton');
+
+//add event listener
+scanBtn.addEventListener('click', function(event) {
+  window.location.href='Students.html';
+});
